72e3ab85c60f65274caec7aa2526f1f02187bc30
guillemrull111 <guillem.rull@estudiant.enti.cat>
2026-02-08 10:24:25 +0100

    Menu

---
+++ Maquina-enigma/Maquina-enigma/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main() {
+
+	int InputUsuario;
+
+	std::cout << "         ENIGMA:" << std::endl;
+	std::cout << "-------------------------" << std::endl;
+	std::cout << "   1. Xifrar missatge" << std::endl;
+	std::cout << "   2. Desxifrar missatge" << std::endl;
+	std::cout << "   3. Editar rotors" << std::endl;
+	std::cout << "   4. Sortir" << std::endl;
+	std::cout << "\nQue vols fer: ";
+	std::cin >> InputUsuario;
+
+	if (InputUsuario == 1) // Xifrar missatge
+	{
+
+	}
+
+	else if (InputUsuario == 2) // Desxifrar missatge
+	{
+
+	}
+
+	else if (InputUsuario == 3) // Editar rotors
+	{
+
+	}
+
+	else if (InputUsuario == 4) // Sortir
+	{
+		return 0;
+	}
+}
