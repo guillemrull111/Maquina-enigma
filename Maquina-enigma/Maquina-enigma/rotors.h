1a71addd8054f2e7583c687f5d1139c55e7ab299
N3BUR4G <ruben.alvarez@estudiant.enti.cat>
2026-02-11 13:40:18 +0100

    moviment rotors + txt

--- Maquina-enigma/Maquina-enigma/rotors.h
+++ Maquina-enigma/Maquina-enigma/rotors.h
@@ -1,16 +1,6 @@
 #pragma once
 
 #include <string>
-
-std::string rotorWiring[3];
-std::string rotorInv[3];
-char rotorNotch[3];
-int rotorPos[3];
-
-bool cargarRotors();
-void setVentana(char a, char b, char c);
-void stepRotors();
-char pasarForward(char c);
-char pasarBackward(char c);
 void editarRotors();
+void movimentRotors(int& posicioEsquerra, int& posicioCentral, int& posicioDreta, char notchEsquerra, char notchCentral);
 
